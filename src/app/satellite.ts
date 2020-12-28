fa2de0a72135e9232456c93bc6e9a3787c9df2f8
abidemi ajidahun <a_ajidahun@yahoo.com>
2020-12-28 17:36:56 -0600

    final without bonus

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,11 +4,17 @@ orbitType: string;
 type: string;
 operational: boolean;
 launchDate: string;
-constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+
+constructor(name: string, type: string, operational: boolean, orbitType: string, launchDate: string){
     this.name = name;
     this.orbitType = orbitType;
     this.type = type;
     this.operational = operational;
     this.launchDate = launchDate;
+}
+shouldShowWarning(): boolean {
+    if(this.type === "Space Debris"){
+        return true;
+    }
 }
 }
