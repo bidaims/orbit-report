e8f785ba1e1906d746aa60195eb29c5cad2806cd
abidemi ajidahun <a_ajidahun@yahoo.com>
2020-12-28 23:00:22 -0600

    final with some bonus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { OrbitListComponent } from './orbit-list/orbit-list.component';
+import { OrbitCountsComponent } from './orbit-counts/orbit-counts.component';
 
 @NgModule({
-  declarations: [AppComponent, OrbitListComponent],
+  declarations: [AppComponent, OrbitListComponent, OrbitCountsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
